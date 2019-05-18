982888def626495e5ba9be42dd08145b33d914c2
Prasanna D <prasanna.dhakshinamoorthy@mattel.com>
2019-05-18 23:05:24 +0530

    Initial Commit

---
+++ Aware/Aware.h
@@ -0,0 +1,19 @@
+//
+//  Aware.h
+//  Aware
+//
+//  Created by admin on 18/05/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Aware.
+FOUNDATION_EXPORT double AwareVersionNumber;
+
+//! Project version string for Aware.
+FOUNDATION_EXPORT const unsigned char AwareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aware/PublicHeader.h>
+
+
